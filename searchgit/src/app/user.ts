e63d09f97fa751eadc808c9450e122617f935974
tinatasha <tashambiti@gmail.com>
2019-07-15 12:06:27 +0300

    add pipe

---
+++ searchgit/src/app/user.ts
@@ -0,0 +1,6 @@
+export class User {
+constructor(public name:string,public avatar:any,public followers:number,public following:number,public bio:string,
+    public url:string,public repos:number, public created_at:string) {
+    }
+    
+  }
