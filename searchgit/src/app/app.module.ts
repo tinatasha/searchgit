c5d8bf977668f44f9946c892b8fe3e4d3a5fbade
tinatasha <tashambiti@gmail.com>
2019-07-15 12:02:19 +0300

    add servoce

--- searchgit/src/app/app.module.ts
+++ searchgit/src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { RepositoryComponent } from './repository/repository.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ProfileService } from './profile.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DatetimePipe } from './datetime.pipe';
+import { RepoDetailsComponent } from './repo-details/repo-details.component';
+import { FormsModule} from '@angular/forms';
+
+
+const routes:Routes=[
+  {path:"repository",component:RepositoryComponent},
+  {path:"user",component:UserComponent},
+  {path:"profile",component:RepoDetailsComponent},
+  {path:"",redirectTo:"/user",pathMatch:"full"},
+  {path:'**',component:NotFoundComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    RepositoryComponent,
+    NotFoundComponent,
+    DatetimePipe,
+    RepoDetailsComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes),
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
